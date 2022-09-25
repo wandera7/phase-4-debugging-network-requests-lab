ed89ce34532d0c0b3c6023cddd33a6a20fe40f38
wandera7 <julius.wandera@student.moringaschool.com>
2022-09-25 20:40:06 +0300

    Done.

--- app/controllers/toys_controller.rb
+++ app/controllers/toys_controller.rb
@@ -1,5 +1,8 @@
 class ToysController < ApplicationController
   wrap_parameters format: []
+  rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+  rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity_response
+
 
   def index
     toys = Toy.all
@@ -7,13 +10,14 @@ class ToysController < ApplicationController
   end
 
   def create
-    toy = Toys.create(toy_params)
+    toy = Toy.create!(toy_params)
     render json: toy, status: :created
   end
 
   def update
     toy = Toy.find_by(id: params[:id])
-    toy.update(toy_params)
+    toy.update!(toy_params)
+    render json: toy
   end
 
   def destroy
@@ -23,9 +27,17 @@ class ToysController < ApplicationController
   end
 
   private
-  
+
   def toy_params
     params.permit(:name, :image, :likes)
+  end
+
+  def render_not_found_response
+    render json: { error: "Toy not found" }, status: :not_found
+  end
+
+  def render_unprocessable_entity_response(invalid)
+    render json: { errors: invalid.record.errors.full_messages }, status: :unprocessable_entity
   end
 
 end
--- app/models/toy.rb
+++ app/models/toy.rb
@@ -1,2 +1,4 @@
 class Toy < ApplicationRecord
+  validates :name, presence: true, uniqueness: true
+  validates :image, presence: true, uniqueness: true
 end
